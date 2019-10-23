9aad4f1b3aea877e41b83d76fce6a1fb7a4aa71f
Jacob2148 <jtd145@scarletmail.rutgers.edu>
2019-10-23 12:06:31 -0400

    assignment 2 (unfinished)

---
+++ Expl2.py
@@ -0,0 +1,18 @@
+num = (["1, 2, 3, 4, 5"])
+print num
+def sign(num):
+    if type(num) != list:
+        raise TypeError("not a list")
+    for element in num:
+            if element = '+'
+                return '+'
+
+
+
+
+#defined function to label if the numbers in the list are positive
+  answer = '?'
+  if num > 0:
+    answer = '+'
+  if num < '4'
+    answer = '+'
