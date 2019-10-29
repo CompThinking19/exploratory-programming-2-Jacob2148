cfc8445780fc053afbe550fc8c12e8d8976ab67e
Jacob2148 <jtd145@scarletmail.rutgers.edu>
2019-10-29 11:10:48 -0400

    expl program 2
    completed assignment

--- Expl2.py
+++ Expl2.py
@@ -1,18 +1,14 @@
-num = (["1, 2, 3, 4, 5"])
-print num
 def sign(num):
     if type(num) != list:
         raise TypeError("not a list")
+    x = 0
     for element in num:
-            if element = '+'
-                return '+'
+        if element > 0:
+            x += 1
+    return x
+sign([1, 2, 3, 4,5])
+sign("1, 2, 3, 4, 5")
 
-
-
-
+#labeling num as coexisting by a string name as well
 #defined function to label if the numbers in the list are positive
-  answer = '?'
-  if num > 0:
-    answer = '+'
-  if num < '4'
-    answer = '+'
+  
